7430cbe39a0d92d18d3b1824e969e9c45b6e1449
palakdevgan <m7.palak@gmail.com>
2022-04-27 13:40:16 -0400

    created a web server

--- server.js
+++ server.js
@@ -3,6 +3,12 @@ const { animals } = require('./data/animals');
 const PORT = process.env.PORT || 3001;
 const app = express();
 
+function findById(id, animalsArray) {
+    const result = animalsArray.filter(animal => animal.id === id)[0];
+    return result;
+  }
+
+
 function filterByQuery(query, animalsArray) {
     let personalityTraitsArray = [];
     // Note that we save the animalsArray as filteredResults here:
@@ -52,6 +58,18 @@ app.get('/api/animals',(req,res) =>{
     }
     res.json(results);
 });
+
+app.get('/api/animals/:id', (req, res) => {
+    const result = findById(req.params.id, animals);
+      if (result) 
+      {
+       res.json(result);
+      } 
+      else 
+      {
+        res.send(404);
+       }
+  });
 
 app.listen(PORT, () =>{
     console.log(`API server now on port 3001!`);
