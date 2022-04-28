45f88030ab9850de9860196658ddafeb424af9ef
palakdevgan <m7.palak@gmail.com>
2022-04-28 14:21:38 -0400

    Implemented Front end

--- server.js
+++ server.js
@@ -5,6 +5,7 @@ const { animals } = require('./data/animals');
 const PORT = process.env.PORT || 3001;
 const app = express();
 
+app.use(express.static('public'));
 // parse incoming string or array data
 app.use(express.urlencoded({ extended: true }));
 // parse incoming JSON data
@@ -120,6 +121,22 @@ app.post('/api/animals', (req, res) => {
     res.json(animal);
     }
 });
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/index.html'));
+  });
+
+app.get('/animals', (req, res) => {
+  res.sendFile(path.join(__dirname, './public/animals.html'));
+}); 
+
+app.get('/zookeepers', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/zookeepers.html'));
+  });
+
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/index.html'));
+  });
 
 app.listen(PORT, () =>{
     console.log(`API server now on port 3001!`);
